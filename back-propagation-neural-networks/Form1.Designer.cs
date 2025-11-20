f2a78cfd0b0c19965a5508b31c0a78509531799d
Hermoine <zhermoine@gmail.com>
2025-11-21 01:31:50 +0800

    Finished adjusting epochs and hidden layers

--- back-propagation-neural-networks/Form1.Designer.cs
+++ back-propagation-neural-networks/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox5 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(67, 33);
+            textBox1.MinimumSize = new Size(0, 30);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(272, 30);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(67, 85);
+            textBox2.MinimumSize = new Size(0, 30);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(272, 30);
+            textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10F);
+            button1.Location = new Point(94, 282);
+            button1.Name = "button1";
+            button1.Size = new Size(154, 50);
+            button1.TabIndex = 3;
+            button1.Text = "Create BPNN";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 10F);
+            button2.Location = new Point(313, 331);
+            button2.Name = "button2";
+            button2.Size = new Size(154, 50);
+            button2.TabIndex = 4;
+            button2.Text = "Train NN";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 10F);
+            button3.Location = new Point(553, 282);
+            button3.Name = "button3";
+            button3.Size = new Size(154, 50);
+            button3.TabIndex = 5;
+            button3.Text = "Test";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(454, 139);
+            textBox5.MinimumSize = new Size(0, 30);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(272, 30);
+            textBox5.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(67, 139);
+            textBox3.MinimumSize = new Size(0, 30);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(272, 30);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(67, 195);
+            textBox4.MinimumSize = new Size(0, 30);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(272, 30);
+            textBox4.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 410);
+            label1.Name = "label1";
+            label1.Size = new Size(310, 20);
+            label1.TabIndex = 9;
+            label1.Text = "NOTE: Create BPNN first before anything else.";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Back Propagation Neural Network";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private Label label1;
     }
 }
--- back-propagation-neural-networks/Form1.cs
+++ back-propagation-neural-networks/Form1.cs
@@ -1,10 +1,147 @@
+using Backprop;
+
 namespace back_propagation_neural_networks
 {
     public partial class Form1 : Form
     {
+        NeuralNet nn;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNet(4, 1, 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 3900; x++)
+            {
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.setInputs(0, Convert.ToDouble(textBox3.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox4.Text));
+            nn.run();
+            textBox5.Text = "" + nn.getOuputData(0);
         }
     }
 }
