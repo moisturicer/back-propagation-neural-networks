6936a897354935378d05e0897bd2c304b26866d8
Hermoine <zhermoine@gmail.com>
2025-11-20 23:38:38 +0800

    Initial commit

---
+++ back-propagation-neural-networks/Form1.cs
@@ -0,0 +1,10 @@
+namespace back_propagation_neural_networks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
